2b561787d65409b144fc0c393292a519bbd2c1b7
Your Name <iqratsm004@gmail.com>
2025-10-20 20:39:34 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 connectDB();
